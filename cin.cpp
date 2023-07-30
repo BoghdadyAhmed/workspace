c7c4d2ac6b304604584b462d6cd1310b4074b932
BoghdadyAhmed <132693797+BoghdadyAhmed@users.noreply.github.com>
2023-07-30 17:35:28 +0300

    .

--- cin.cpp
+++ cin.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int plusFuncInt(int x, int y) {
+int plusFuncInt(int x, int y)
+{
   return x + y;
 }
 
-double plusFuncDouble(double x, double y) {
+double plusFuncDouble(double x, double y)
+{
   return x + y;
 }
 
-int main() {
-  
-  cout << "Int: " << plusFuncInt(10,15) << "\n";
-  cout << "Double: " << plusFuncDouble(10.5,12.8) << endl;
+int main()
+{
+
+  cout << "Int: " << plusFuncInt(10, 15) << "\n";
+  cout << "Double: " << plusFuncDouble(10.5, 12.8) << endl;
   return 0;
 }
